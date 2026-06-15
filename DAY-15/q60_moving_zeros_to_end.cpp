3c20c5dac28ccda4637b40afcb2ce413e4391101
Devvrat Kumar <devvrat1717@gmail.com>
2026-06-15 10:13:24 +0530

    DAY-15 programe uploaded

---
+++ DAY-15/q60_moving_zeros_to_end.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int j = 0;
+
+    // Move non-zero elements to the front
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    // Fill remaining positions with zeros
+    while(j < n) {
+        arr[j] = 0;
+        j++;
+    }
+
+    cout << "Array after moving zeros to end: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
