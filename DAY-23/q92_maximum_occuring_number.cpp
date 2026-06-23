24364cea3711bd3a6d7f0938b534cc796479c96b
Devvrat Kumar <devvrat1717@gmail.com>
2026-06-23 21:15:01 +0530

    DAY-23 programe uploaded

---
+++ DAY-23/q92_maximum_occuring_number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxCount = 0, maxElement;
+
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            maxElement = arr[i];
+        }
+    }
+
+    cout << "Maximum occurring element = " << maxElement << endl;
+    cout << "Frequency = " << maxCount;
+
+    return 0;
+}
